304e7976cb80b9fd1729c73ae9dba42bbb5932bf
Tanbobobo <96757060+Tanbobobo@users.noreply.github.com>
2021-12-31 04:08:54 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import sys
 import sphinx_rtd_theme
 import mock
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../demos/'))
  
 
 MOCK_MODULES = ['numpy', 'sklearn', 'pandas','timm','opencv-python','torch','PIL'] 
