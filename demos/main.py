6c31af2868ec50e16cc4aa3498d9208b131f0001
Tanbobobo <96757060+Tanbobobo@users.noreply.github.com>
2022-01-03 13:53:18 +0800

    Update main.py

--- demos/main.py
+++ demos/main.py
@@ -8,7 +8,7 @@ from albumentations import *
 from models import resnet
 from datetime import datetime
 
-import torch.nn as nn
+import wandb
 def run():
     r'''
 
@@ -54,45 +54,51 @@ def run():
     Returns: Happiness :)
 
     '''
+    wandb.init(project='demo')
+    config = wandb.config
     time_info = str(datetime.now()).replace(' ', '_').replace(':', '_').split('.')[0]
     SAVE_DIR = "./work_dirs/" + f"{time_info}/"
     os.makedirs(SAVE_DIR, exist_ok=True)
     weight_name = 'epoch_{}.pth'
     log_name = 'log.txt'
-    epochs = 10
+    config.epochs = 10
     device = 'cpu'  # 'cuda:0'
     fold_num = 1
-    data_seed = 2021
-    data_root = './data/fundus_data/'
+    config.data_seed = 2021
+    config.data_root = './data/fundus_data/'
     img_folder = 'imgs'
     anns_folder = f'split_seed_{data_seed}/fold_{fold_num}'
     anns_name_train = 'train_gt.csv'
     anns_name_val = 'eval_gt.csv'
-    anns_dict = {'non': 0, 'early': 1, 'mid_advanced': 2}
-    depth = 18
+    config.anns_dict = {'non': 0, 'early': 1, 'mid_advanced': 2}
+    config.depth = 18
     pretrained = False
-    cls_num = len(anns_dict)
-    mean = (0.485, 0.456, 0.406)  # RGB
-    std = (0.229, 0.224, 0.225)
-    img_h = 512
-    img_w = 512
-    lr = 1e-4
-    batch_size = 4
+    config.cls_num = len(anns_dict)
+    config.mean = (0.485, 0.456, 0.406)  # RGB
+    config.std = (0.229, 0.224, 0.225)
+    config.img_h = 512
+    config.img_w = 512
+    config.lr = 1e-4
+    config.batch_size = 4
     shuffle = True
     pin_memory = True
     num_workers = 0
     weight_decay = 1e-5
     model = resnet(
-        depth=depth,
-        cls_num=cls_num,
+        depth=config.depth,
+        cls_num=config.cls_num,
         pretrained=pretrained
     )
-    optim = adam_optim(lr=lr, params=model.parameters(), weight_decay=weight_decay)
+    config.model = model.__class__.__name__
+    optim = adam_optim(lr=config.lr, params=model.parameters(), weight_decay=weight_decay)
+    config.optimizer = optim.__class__.__name__
     crit = crossentropy
+    config.loss = crit.__class__.__name__
     metric = kappa()
+    config.metric = metric.__class__.__name__
     transforms = Compose(
         [
-            Resize(img_h, img_w)
+            Resize(config.img_h, config.img_w)
         ]
     )
     dataset_train = GAMMADataset(
@@ -106,13 +112,13 @@ def run():
             img_folder
         ),
         transforms=transforms,
-        anns_dict=anns_dict,
-        mean=mean,
-        std=std
+        anns_dict=config.anns_dict,
+        mean=config.mean,
+        std=config.std
     )
     data_loader_train = GAMMALodaer(
         dataset=dataset_train,
-        batch_size=batch_size,
+        batch_size=config.batch_size,
         shuffle=shuffle,
         pin_memory=pin_memory,
         num_workers=num_workers
@@ -128,13 +134,13 @@ def run():
             img_folder
         ),
         transforms=transforms,
-        anns_dict=anns_dict,
-        mean=mean,
-        std=std
+        anns_dict=config.anns_dict,
+        mean=config.mean,
+        std=config.std
     )
     data_loader_val = GAMMALodaer(
         dataset=dataset_val,
-        batch_size=batch_size,
+        batch_size=config.batch_size,
         shuffle=shuffle,
         pin_memory=pin_memory,
         num_workers=num_workers
@@ -142,19 +148,25 @@ def run():
     best_metric_value = 0
     log_format = "epoch: {}, train loss: {}, val loss: {}, train score: {}, val score: {}\n"
 
-    for epoch in range(1, epochs + 1):
-        print(epoch)
+    for epoch in range(1, config.epochs + 1):
+
         model, \
         metric_value_train, \
         loss_value_train = learn(optimizer=optim, criterion=crit, metric=metric,
                                  loader=data_loader_train,
-                                 model=model, device=device
+                                 model=model, device=device,
+
                                  )
         model, \
         metric_value_val, \
         loss_value_val = val(criterion=crit, metric=metric,
                              loader=data_loader_val, model=model,
                              device=device)
+
+        wandb.log({'train_loss_epoch':loss_value_train,
+                   'train_score_epoch':metric_value_train,
+                   'val_loss_epoch':loss_value_train,
+                   'val_score_epoch':metric_value_val})
         if 1:  # metric_value_val > best_metric_value:
             best_metric_value = metric_value_val
             torch.save(
